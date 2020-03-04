07669e7fce6f1255d8615c0cd74123faccee87fb
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2020-03-04 23:10:30 +0000

    Added (as yet untested) code for a differentiable shapelet transform.

---
+++ src/shapelet_transform.py
@@ -0,0 +1,240 @@
+import torch
+
+
+def _restriction(times, path, start, end):
+    """Differentiably restricts a piecewise linear path.
+    
+    The arguments `times` and `path` between them define a piecewise linear function f, with
+    f(times[i]) = path[..., i, :] for each i, and affine on the pieces in between.
+    
+    The return value is this piecewise linear function restricted to the interval [start, end], represented in the same
+    way by a pair of (times, path).
+    
+    Arguments:
+        times: A 1D tensor of shape (length,), describing the times of each knot.
+        path: A tensor of shape (..., length, channels), describing the values of each knot.
+        start: The start time to restrict to.
+        end: The end time to restrict to.
+
+    Returns:
+        A 2-tuple of (restricted_times, restricted_path), where restricted_times is a 1D tensor of shape (r_length,) and
+        restricted_path is a tensor of shape(..., r_length, channels).
+    """
+    
+    times = torch.as_tensor(times)
+    path = torch.as_tensor(path)
+    start = torch.as_tensor(start)
+    end = torch.as_tensor(end)
+    
+    assert len(times.shape) == 1, "times must be a 1D tensor of shape (length,)."
+    assert len(path.shape) >= 2, "path must be a tensor of shape(..., length, channels)."
+    assert len(start.shape) == 0, "start and end must be scalars."
+    assert len(end.shape) == 0, "start and end must be scalars."
+    assert path.size(-2) == times.size(0), "times and path must have the same size length dimension."
+    assert times.size(0) >= 2, "Length dimension must be of size at least 2 to define a path."
+    assert start >= times[0], "start and end must be within the interval specified by times."
+    assert end <= times[-1], "start and end must be within the interval specified by times."
+    assert start < end, "start must be less than end."
+    prev_time = times[0]
+    for time in times[1:]
+        assert time > prev_time, "times must be an increasing sequence."
+        prev_time = time
+    
+    # >= for start and > for end is deliberate
+    # this correctly handles the cases that start == times[i] or end == times[i] for some i.
+    start_index = (start >= times).sum()
+    end_index = (end > times).sum()
+    
+    before_start_time = times[start_index - 1]
+    after_start_time = times[start_index]
+    start_ratio = (start - before_start_time) / (after_start_time - before_start_time)
+    start_restriction = path[..., start_index - 1] + start_ratio * (path[..., start_index, :] - path[..., start_index - 1, :])
+    
+    middle_restriction = path[..., start_index:end_index, :]
+    
+    before_end_time = times[end_index]
+    after_end_time = times[end_index + 1]
+    end_ratio = (end - before_end_time) / (after_end_time - before_end_time)
+    end_restriction = path[..., end_index, :] + end_ratio * (path[..., end_index + 1, :] - path[..., end_index, :])
+    
+    restricted_times = torch.cat([start, times[start_index:end_index], end], dim=0)
+    restricted_path = torch.cat([start_restriction.unsqueeze(-2), middle_restriction, end_restriction.unsqueeze(-2)],
+                                dim=-2)
+    return restricted_times, restricted_path
+   
+    
+def _continuous_min(start, end, fn, max_sampling_gap=None):
+    """Differentiably calculates
+    ```
+    g(start, end) = \min_{s \in [start, end]} fn(s)
+    ```
+
+    In practice this is done by sampling many points in the region [start, end] and calculating the minimum over all of
+    them. (And handling 'start' and 'end' specially so that they may be differentiated though.) The maximum tolerance
+    for the gap between points is controlled by `max_sampling_gap`.
+
+    Arguments:
+        start: A scalar.
+        end: A scalar.
+        fn: A function which consumes a scalar and returns an any-dimensional tensor.
+        max_sampling_gap: Either None (=1000 sample points) or the maximum gap between two sample points; i.e. we
+            compute a minimum over s in {start, start + epsilon, start + 2 * epsilon, ..., end - epsilon, end} where
+            epsilon < max_sampling_gap.
+
+    Returns:
+        A PyTorch tensor of the same shape as `fn` returns.
+    """
+    start = torch.as_tensor(start)
+    end = torch.as_tensor(end)
+    start_detached = start.detach()
+    end_detached = end.detach()
+    
+    assert start < end, "start must be less than end."
+    assert max_sampling_gap > 0, "max_sampling_gap must be positive."
+    
+    if max_sampling_gap is None:
+        num_samples = 1000
+    else:
+        max_sampling_gap = torch.as_tensor(max_sampling_gap)
+        assert not max_sampling_gap.requires_grad, "Cannot differentiate with respect to the number of sample points."
+        num_samples = 1 + torch.ceil((end_detached - start_detached) / max_sampling_gap)
+    points = torch.linspace(start_detached, end_detached, num_samples)
+    
+    min_result_middle = torch.stack([fn(point) for point in points[1:-1]], dim=0).min(dim=0)
+    # This allows us to differentiate through the endpoints
+    # If we wanted we could also construct every point in points[1:,-1] differentiably, but we're aiming for a
+    # continuous minimum here, and that's just an implementation detail. (Basically it depends on whether you use a
+    # discretise-then-optimise or optimise-then-discretise approach.)
+    min_result = torch.stack([fn(start), min_result_middle, fn(end)], dim=0).min(dim=0)
+    return min_result
+
+
+def _add_knots(times, path, additional_times):
+    """Adds knots to a piecewise linear path, such that it doesn't change the function it represents.
+
+    Arguments:
+        times, path: As `_restriction`.
+        additional_times: Additional times to add knots to by linearly interpolating between the values already given.
+
+    Returns:
+        A tensor of shape (..., times.size(0) + additional_times.size(0), path.size(-1)).
+    """
+    times = torch.as_tensor(times)
+    path = torch.as_tensor(path)
+    additional_times = torch.as_tensor(additional_times)
+
+    prev_time = times[0]
+    for time in times[1:]:
+        assert time > prev_time, "times must be increasing"
+        prev_time = time
+    prev_additional_time = additional_times[0]
+    for additional_time in additional_times[1:]:
+        assert additional_time > prev_additional_time, "additional_times must be increasing"
+        prev_additional_time = additional_time
+    assert additional_times[0] >= times[0], "additional_times cannot go outside the interval [times[0], times[-1]]"
+    assert additional_times[-1] <= times[-1], "additional_times cannot go outside the interval [times[0], times[-1]]"
+
+    prev_time_index = 0
+    iter_times = iter(times)
+    prev_time = next(iter_times)
+    next_time = next(iter_times)
+    new_path = [path[..., 0, :]]
+    new_times = [times[0]]
+    if additional_times[0] == times[0]:
+        additional_times = additional_times[1:]
+    for additional_time in additional_times:
+        while additional_time > next_time:
+            prev_time_index += 1
+            new_times.append(times[prev_time_index])
+            new_path.append(path[..., prev_time_index, :])
+            prev_time = next_time
+            next_time = next(iter_times)
+        if additional_time == next_time:
+            continue
+        ratio = (additional_time - prev_time) / (next_time - prev_time)
+        path_at_additional_time = path[..., prev_time_index, :] + ratio * (path[..., prev_time_index + 1, :] - path[..., prev_time_index, :])
+        new_times.append(additional_time)
+        new_path.append(path_at_additional_time)
+    for _ in iter_times:
+        prev_time_index += 1
+        new_times.append(times[prev_time_index])
+        new_path.append(path[..., prev_time_index, :])
+    return torch.stack(new_times, dim=0), torch.stack(new_path, dim=-2)
+  
+    
+class ShapeletTransform(torch.nn.Module):
+    """Applies a generalised shapelet transform.
+
+    Each shapelet that it is compared against will be a piecewise linear function.
+    """
+    def __init__(self, in_channels, num_shapelets, num_shapelet_samples, discrepancy_fn,  max_shapelet_length,
+                 max_sampling_gap=None):
+        """
+        Arguments:
+            in_channels: An integer specifying the number of input channels in the path it will be called with.
+            num_shapelets: How many shapelets to compare the path against.
+            num_shapelet_samples: How finely each shapelet will be discretised, i.e. how many knots it has.
+            discrepancy_fn: The function measuring the similarity or discrepancy between two paths. This function should
+                take three arguments, call them `times`, `path1`, `path2`. Then `times` and `path1` between them
+                describe the unique continuous piecwise affine path `f` such that f(times[i]) = path1[i] for all i,
+                whilst `times` and `path2` similarly define the unique continuous piecewise affine path `g` such that
+                g(times[i]) = path2[i]. `times` will be a 1D tensor of shape (length,), whilst `path1` and `path2`
+                should be tensors of shape (..., length, channels) and (length, channels) respectively, where '...'
+                '...' represents some non-negative number of batch dimensions. It should return a tensor of shape (...,)
+                describing the similarity between `path1` and `path2`.
+            max_shapelet_length: The maximum length for a shapelet. (As if it grows too long then it cannot be compared
+                against.)
+            max_sampling_gap: As `continuous_min`.
+        """
+        # discrepancy_fn accepts two tensors:
+        #   a restricted_path of shape (..., restricted_length, in_channels)
+        #   a shapelet of shape (num_shapelet_samples, in_channels)
+        # and should return a tensor of shape (...,).
+        
+        super(Shapelet, self).__init__()
+        
+        self.in_channels = in_channels
+        self.num_shapelets = num_shapelets
+        self.num_shapelet_samples = num_shapelet_samples
+        self.discrepancy_fn = discrepancy_fn
+        self.max_sampling_gap = max_sampling_gap
+        self.max_shapelet_length = max_shapelet_length
+
+        self.lengths = torch.nn.Parameter(torch.empty(num_shapelets))
+        self.shapelets  = torch.nn.Parameter(torch.empty(num_shapelets, num_shapelet_samples, in_channels))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        with torch.no_grad():
+            self.lengths.uniform_(self.max_shapelet_length / 2, self.max_shapelet_length)
+            self.shapelets.uniform_(-1, 1)
+
+    def clip_length(self):
+        """Clips the length of the shapelets to valid values. Should be called after every backward pass."""
+        self.lengths.clamp_(0.01, self.max_shapelet_length)
+        
+    def forward(self, times, path):
+        # times is of shape (length,)
+        # path is of shape (..., length, in_channels)
+        
+        assert self.lengths.min() > times[-1], "Time series is too short for shapelet."
+        assert path.size(-1) == self.in_channels, ("Wrong number of input channels. Expected {}, got {}"
+                                                   "".format(self.in_channels, path.size(-1)))
+                                                   
+        discrepancies = []
+        for length, shapelet in zip(self.lengths, self.shapelets):
+            def min_fn(point):
+                # restricted path is of shape (..., restricted_length, in_channels)
+                restricted_times, restricted_path = _restriction(times, path, point, point + length)
+                restricted_times = restricted_times - point  # TODO: reflect this in the LaTeX
+                shapelet_times = torch.linspace(0, length, self.num_shapelet_samples)
+                # normalise both the path and the shapelet to have knots at the same points as each other
+                mutual_times, restricted_path = _add_knots(restricted_times, restricted_path, shapelet_times)
+                _, shapelet = _add_knots(shapelet_times, shapelet, restricted_times)
+                # this will then return a tensor of shape (...,)
+                return discrepancy_fn(mutual_times, restricted_path, shapelet)
+            discrepancy = _continuous_min(times[0], times[-1] - length, min_fn, self.max_sampling_gap)
+            discrepancies.append(discrepancy)
+        # returns a tensor of shape (..., num_shapelets)
+        return torch.stack(discrepancies, dim=-1)
